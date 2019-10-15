aa76b8f9aae9a85af461bd12e4a96ee053074a2d
AnthonyStseshyts <a.stseshyts@mail.ru>
2019-10-15 10:46:30 +0300

    finished the add to cart action

--- src/components/order-cart-table.js/order-cart-table.js
+++ src/components/order-cart-table.js/order-cart-table.js
@@ -4,11 +4,11 @@ import './order-cart-table.css';
 
 const OrderCartTable = ( {items, totalTime, onIncrease, onDecrease, onDelete} ) => {
     const renderRow = (item, idx) => {
-        const { id, name, count, total} = item;
+        const { id, title, count, total} = item;
         return (
         <tr key = {id}>
             <td>{idx + 1}</td>
-            <td>{name}</td>
+            <td>{title}</td>
             <td>{count}</td>
             <td>{total}</td>
             <td>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,38 @@ const initialState = {
   pizzas: [],
   loading: true,
   errors: [],
-  cartItems: [
-    {
-      name: 'abc',
-      id: 1,
-      count: 10,
-      total: 150
-    },
-    {
-      name: 'abc',
-      id: 2,
-      count: 10,
-      total: 150
-    }
-  ],
+  cartItems: [],
   totalTime: 10
+};
+
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [
+      ...cartItems,
+      item
+    ];
+  }
+  return [
+    ...cartItems.slice(0, idx),
+    item,
+    ...cartItems.slice(idx + 1),
+  ];
+};
+
+const updateCartItem = (pizza, item = {}) => {
+
+  const { id = pizza.id, 
+          count = 0, 
+          title = pizza.pizza_name, 
+          total = 0} = item;
+          
+
+  return {
+    id,
+    title,
+    count: count + 1,
+    total: total + pizza.cooking_time
+  };
 };
 
 const reducer = (state = initialState, action) => {
@@ -34,13 +51,26 @@ const reducer = (state = initialState, action) => {
         return {
           ...state,
           loading: true
-        }
+        };
         case 'FETCH_MENU_FAILURE':
             return {
               ...state,
               loading: false,
               errors: action.payload
-            }
+            };
+
+        case 'PIZZA_ADDED_TO_CART':
+          const pizzaId = action.payload;
+          const pizza = state.pizzas.find((pizza) => pizza.id === pizzaId);
+          const itemIndex = state.cartItems.findIndex(({id}) => id === pizzaId);
+          const item = state.cartItems[itemIndex];
+
+          const newItem = updateCartItem(pizza, item);
+          return {
+            ...state,
+            cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
+          };
+
     default:
       return state;
   }
